4a6440f72b7c456ad40a925ac774f0f3948a9905
Anisha Dahal <anisha.dahal@webo.digital>
2022-07-12 18:22:08 +0545

    go basics

---
+++ 13switch-case/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Switch case")
+
+	rand.Seed(time.Now().UnixNano())
+	diceNumber := rand.Intn(6) + 1
+	fmt.Println("Value of dice is ", diceNumber)
+
+	switch diceNumber {
+	case 1:
+		fmt.Println("Dice value is 1 and you can open")
+	case 2:
+		fmt.Println("You can move 2 spot")
+	case 3:
+		fmt.Println("You can move 3 spot")
+		fallthrough
+	case 4:
+		fmt.Println("You can move 4 spot")
+		fallthrough
+	case 5:
+		fmt.Println("You can move 5 spot")
+	case 6:
+		fmt.Println("You can move 6 spot and roll the dice again")
+	default:
+		fmt.Println("Jhyali")
+	}
+}
