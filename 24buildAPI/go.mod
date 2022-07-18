bba1b370b14bfd8473f94333d3a9642d95d3a48f
Anisha Dahal <anisha.dahal@webo.digital>
2022-07-18 12:16:04 +0545

    add routes

--- 24buildAPI/go.mod
+++ 24buildAPI/go.mod
@@ -1,3 +1,5 @@
 module github.com/Anishadahal/buildAPI
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- 24buildAPI/main.go
+++ 24buildAPI/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
 	"time"
-
-	"github.com/gorilla/mux"
 )
 
 //model for course - file
@@ -35,6 +35,22 @@ func (c *Course) isEmpty() bool {
 
 func main() {
 	fmt.Println("Building API")
+	r := mux.NewRouter()
+
+	//seeding
+	courses = append(courses, Course{CourseId: "2", CourseName: "ReactJS", CoursePrice: 299, Author: &Author{Fullname: "Anisha", Website: "anisha.dev"}})
+	courses = append(courses, Course{CourseId: "3", CourseName: "MERN", CoursePrice: 599, Author: &Author{Fullname: "ANsa", Website: "mern.dev"}})
+
+	//routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/create", createOneCourse).Methods("POST")
+	r.HandleFunc("/update/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/delete/{id}", deleteOneCourse).Methods("DELETE")
+
+	//listen to port
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 //controllers -file
@@ -65,7 +81,6 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		json.NewEncoder(w).Encode("No course found with given id.")
-		return
 	}
 }
 
@@ -73,17 +88,32 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Create one course")
 	w.Header().Set("Content-Type", "application/json")
 
-	//what if: body of response is empty
-	if r.Body == nil {
-		json.NewEncoder(w).Encode("Please send some data")
+	var c Course
+	err := json.NewDecoder(r.Body).Decode(&c)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	//what about: { }
-	var course Course
-	_ = json.NewDecoder(r.Body).Decode(&course)
-	if course.isEmpty() {
+	//empty request: {}
+	if c.isEmpty() {
 		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+
+	//TODO: Check only if title is duplicate
+	//loop, title matches with course.coursename, send JSON response
+	for _, course := range courses {
+		if course.CourseName == c.CourseName {
+			json.NewEncoder(w).Encode("This course already exists")
+			return
+		}
+	}
+
+	//what if: body of response is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
 		return
 	}
 
@@ -91,10 +121,9 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	//append course into courses
 
 	rand.Seed(time.Now().UnixNano())
-	course.CourseId = strconv.Itoa(rand.Intn(100))
-	courses = append(courses, course)
-	json.NewEncoder(w).Encode(course)
-	return
+	c.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, c)
+	json.NewEncoder(w).Encode(c)
 }
 
 func updateOneCourse(w http.ResponseWriter, r *http.Request) {
@@ -119,11 +148,11 @@ func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-
+	json.NewEncoder(w).Encode("No course found with given id.")
 	//TODO: Send a response when id is not found
 }
 
-func deleteOneCourse(w http.ResponseWriter, r http.Request) {
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Delete one course")
 	w.Header().Set("Content-Type", "application/json")
 
@@ -131,6 +160,7 @@ func deleteOneCourse(w http.ResponseWriter, r http.Request) {
 	for index, course := range courses {
 		if course.CourseId == params["id"] {
 			courses = append(courses[:index], courses[index+1:]...)
+			//TODO: send a confirm/deny response
 			json.NewEncoder(w).Encode("Deleted successfully")
 			break
 		}
